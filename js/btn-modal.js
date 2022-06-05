92acb73aee8d07188b98b91531f9bb71b04531d3
Marta-HashCash <marta.hashcash.exchange@gmail.com>
2022-06-05 18:40:35 +0300

    for mobile ver commit

--- js/btn-modal.js
+++ js/btn-modal.js
@@ -2,16 +2,16 @@ const ModalContacts = document.querySelector("#contacts-modal");
 const ModalBotLink = document.querySelector("#two-bot-link");
 const ModalDescription = document.querySelector("#three-description");
 const ModalTreaty = document.querySelector("#four-treaty");
-
+const ModalHiddenMenu = document.querySelector('#mobile-menu');
 
 const OpenBtnModalContacts = document.querySelector("#one-contacts-btn");
 const OpenBtnModalBotLink = document.querySelector("#two-bot-link-btn");
 const OpenBtnModalDescription = document.querySelector("#three-description-btn");
 const OpenBtnModalTreaty = document.querySelector("#four-treaty-btn");
+const OpenBtnModalHiddenMenu = document.querySelector('#hidden-menu-btn');
 
-
-const Modals = [ModalContacts, ModalBotLink, ModalDescription, ModalTreaty];
-const Btns = [OpenBtnModalContacts, OpenBtnModalBotLink, OpenBtnModalDescription, OpenBtnModalTreaty];
+const Modals = [ModalContacts, ModalBotLink, ModalDescription, ModalTreaty, ModalHiddenMenu];
+const Btns = [OpenBtnModalContacts, OpenBtnModalBotLink, OpenBtnModalDescription, OpenBtnModalTreaty, OpenBtnModalHiddenMenu];
 
 
 
